c335f7fcc3e46bfd7bf82eea5cf38d32952faf27
rashi2711 <rashigarg803@gmail.com>
2024-11-06 19:38:22 +0530

    Update .gitignore

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 app.use('/', indexRouter);
 app.use('/hisaab', hisaabRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
